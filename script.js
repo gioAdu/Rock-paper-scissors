0208475b1808232c80682d5ace1087a6cc6e90ef
Legates1 <103452467+Legates1@users.noreply.github.com>
2022-06-05 04:07:16 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let hands = ["rock", "paper", "scissors"]
+let playerEl = document.getElementById("player-el")
+let computerEL = document.getElementById("computer-el")
+let resultEl = document.getElementById("result-el")
+let numbersEl = document.getElementById("numbers-el")
+let win = 0
+const vinVisUgebs = {
+	"scissors": "paper",
+	"paper": "rock",
+	"rock": "scissors"}
+
+function RPS(selection) {
+	console.log("aqvar")
+	playerEl.textContent = selection
+	let computerChoice = RPSComputer() ;
+	computerEL.textContent = computerChoice
+	resultEl.textContent = winner(selection,computerChoice) 
+	numbersEl.textContent = win
+}
+
+
+function RPSComputer() {
+    let i = Math.floor(Math.random() * hands.length)
+	return hands[i]
+}
+
+function winner(selection,computerChoice) {
+	if (selection === computerChoice){
+		return "draw"
+	}  
+	if (vinVisUgebs[selection] === computerChoice){
+		win ++
+		return "You win"
+	} 
+	win --
+	return "You lose"
+}
